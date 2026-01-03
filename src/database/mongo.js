d12968d663d6dae1e9941c402d26c65fb13b4b15
GamerKinq <109361490+GamerKinq@users.noreply.github.com>
2026-01-03 22:52:37 +0300

    Update mongo.js

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
     // Bu link SRV (DNS) kullanmaz, doğrudan sunuculara bağlanır
-const mongoURI = "mongodb://arena_user:gamerkinq@cluster0-shard-00-00.ldyqw4g.mongodb.net:27017,cluster0-shard-00-01.ldyqw4g.mongodb.net:27017,cluster0-shard-00-02.ldyqw4g.mongodb.net:27017/arena_bot?ssl=true&authSource=admin&retryWrites=true&w=majority";
+const mongoURI = "MANGO DB";
 
     try {
         await mongoose.connect(mongoURI, {
