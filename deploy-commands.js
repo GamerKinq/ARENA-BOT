fc7f3ed6486bcf1082e1e15a1456a8686656341e
GamerKinq <109361490+GamerKinq@users.noreply.github.com>
2026-01-03 22:57:39 +0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,11 +3,12 @@ const commands = [
     { name: 'arena', description: 'Savaşı başlat!' },
     { name: 'rank', description: 'Sıralamayı gör!' }
 ];
-const rest = new REST({ version: '10' }).setToken('MTM4MDk2NDkxODMyODY5MjgwOA.GQ-ZwV.L4Gk-WfJeDIVbTqlDL1xSa6vLfgXUbs-g3lwSw');
+const rest = new REST({ version: '10' }).setToken('BOT-TOKEN');
 
 (async () => {
     try {
         await rest.put(Routes.applicationGuildCommands('1380964918328692808', '1442333314508783786'), { body: commands });
         console.log('✅ Komutlar yüklendi!');
     } catch (e) { console.error(e); }
+
 })();
