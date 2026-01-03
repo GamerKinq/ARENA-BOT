08b5f29a1132e5b8ff1c89edd861e1cd40c94180
GamerKinq <109361490+GamerKinq@users.noreply.github.com>
2026-01-03 22:48:13 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports =
   "token": "TOKEN-BOT",
   "clientId": "BOT-CLİENT-İD",
   "guildId": "SUNUCU-İD",
-  //"mongoURI": "mongodb://arena_user:gamerkinq@cluster0-shard-00-00.ldyqw4g.mongodb.net:27017,cluster0-shard-00-01.ldyqw4g.mongodb.net:27017,cluster0-shard-00-02.ldyqw4g.mongodb.net:27017/arena_bot?ssl=true&replicaSet=atlas-xxxx-shard-0&authSource=admin&retryWrites=true&w=majority" 
+  //"mongoURI": "MANGO DB" 
 }
